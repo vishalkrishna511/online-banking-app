9b24fdbfb0d2b1645c11ee5401c489c5759dc39b
vaasugambhir <vaasugambhir@gmail.com>
2023-09-12 01:57:07 +0530

    Added: cross origin in custmer controller

--- serverside/src/main/java/com/onlinebanking/serverside/controller/CustomerController.java
+++ serverside/src/main/java/com/onlinebanking/serverside/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.onlinebanking.serverside.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,31 +13,30 @@ import com.onlinebanking.serverside.model.Login;
 import com.onlinebanking.serverside.service.CustomerService;
 import com.onlinebanking.serverside.service.LoginService;
 
-
-
+@CrossOrigin
 @RestController
 public class CustomerController {
-	
+
 	@Autowired
 	CustomerService customerService;
-	
+
 	@Autowired
 	LoginService LoginService;
-	
+
 	@PostMapping("/addCustomer")
 	public ResponseEntity<?> addCustomer(@RequestBody Customer c) {
 
-			Customer response = customerService.save(c);
-			if(response == null) {
-				return ResponseEntity.status(HttpStatus.CONFLICT).body("Customer details Invalid");
-			}
-			return ResponseEntity.status(HttpStatus.OK).body(response);
+		Customer response = customerService.save(c);
+		if (response == null) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).body("Customer details Invalid");
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(response);
 
 	}
-	
+
 	@PostMapping("/login")
 	public String validateCustomer(@RequestBody Login login) {
 		return LoginService.validateCustomer(login);
 	}
-	
+
 }
