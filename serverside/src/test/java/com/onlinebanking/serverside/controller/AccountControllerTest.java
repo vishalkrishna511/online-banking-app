0a5a3a7efaa2eb1b78c6b0066a83faad2f9ee4a7
xerexcoded <arnavbanerjee61@gmail.com>
2023-09-13 21:21:52 +0530

    Start of tests

---
+++ serverside/src/test/java/com/onlinebanking/serverside/controller/AccountControllerTest.java
@@ -0,0 +1,22 @@
+package com.onlinebanking.serverside.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+
+public class AccountControllerTest {
+
+    @Test
+    void addAccount() {
+
+    }
+
+    @Test
+    void viewAccount() {
+    }
+
+    @Test
+    void getAccountDetails() {
+    }
+}
---
+++ serverside/src/test/java/com/onlinebanking/serverside/controller/TransactionControllerTest.java
@@ -0,0 +1,12 @@
+package com.onlinebanking.serverside.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransactionControllerTest {
+
+    @Test
+    void addTransaction() {
+    }
+}
