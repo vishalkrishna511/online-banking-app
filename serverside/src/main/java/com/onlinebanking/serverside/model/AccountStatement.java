c2cc5f9df900c38460148172fd4a4be84e5d5d1e
I-am-SS <91775349+I-am-SS@users.noreply.github.com>
2023-09-18 11:42:01 +0530

    generated getters and setters

--- serverside/src/main/java/com/onlinebanking/serverside/model/AccountStatement.java
+++ serverside/src/main/java/com/onlinebanking/serverside/model/AccountStatement.java
@@ -6,4 +6,17 @@ import lombok.Data;
 public class AccountStatement {
     private String fromDate;
     private String toDate;
+	public String getFromDate() {
+		return fromDate;
+	}
+	public void setFromDate(String fromDate) {
+		this.fromDate = fromDate;
+	}
+	public String getToDate() {
+		return toDate;
+	}
+	public void setToDate(String toDate) {
+		this.toDate = toDate;
+	}
+    
 }
--- serverside/src/main/java/com/onlinebanking/serverside/model/Admin.java
+++ serverside/src/main/java/com/onlinebanking/serverside/model/Admin.java
@@ -23,4 +23,30 @@ public class Admin {
     @Column(nullable = false)
     private String pswd;
 
+	public long getAdminId() {
+		return adminId;
+	}
+
+	public void setAdminId(long adminId) {
+		this.adminId = adminId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPswd() {
+		return pswd;
+	}
+
+	public void setPswd(String pswd) {
+		this.pswd = pswd;
+	}
+    
+    
+
 }
