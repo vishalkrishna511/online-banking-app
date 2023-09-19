ab4ed0a354e9452f1b4fcfa3ddee80ca2f383469
I-am-SS <91775349+I-am-SS@users.noreply.github.com>
2023-09-19 10:06:21 +0530

    Some ui change in show transactions'

--- onlinebanking-ui/src/Components/ShowTransactions.jsx
+++ onlinebanking-ui/src/Components/ShowTransactions.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
-// // import { makeStyles } from "@mui/styles";
-// import { createStyles, makeStyles } from "@mui/styles";
-
+import Box from "@mui/material/Box";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -18,7 +16,7 @@ function ShowTransactions(props) {
   // console.log(userId);
   const [accounts, setAccounts] = useState([]);
   const [transactions, setTransactions] = useState([]);
-  const [selectedAccount, setSelectedAccount] = useState([]);
+  // const [selectedAccount, setSelectedAccount] = useState([]);
 
   useEffect(() => {
     axios
@@ -30,7 +28,7 @@ function ShowTransactions(props) {
       .catch((error) => {
         console.error(error);
       });
-  }, [userName]);
+  }, [userId, userName]);
   useEffect(() => {
     if (accounts.length > 0) {
       let promises = accounts.map((number) =>
@@ -50,96 +48,104 @@ function ShowTransactions(props) {
 
   return (
     <div>
-      {transactions.length > 0 ? (
-        <div style={{ margin: "70px" }}>
-          {transactions.map((txn, index) => (
-            <div key={index}>
-              <h2 style={{ display: "flex", flex: "start" }}>
-                🚩Transactions for Account Number : {accounts[index]}
-              </h2>
-              {txn.length > 0 ? (
-                <TableContainer component={Paper}>
-                  <Table>
-                    <TableHead>
-                      <TableRow>
-                        <TableCell
-                          sx={{
-                            backgroundColor: "#d41c2c",
-                            color: "white",
-                            fontSize: 20,
-                          }}
-                        >
-                          Time Stamp
-                        </TableCell>
-                        <TableCell
-                          sx={{
-                            backgroundColor: "#d41c2c",
-                            color: "white",
-                            fontSize: 20,
-                          }}
-                        >
-                          Amount
-                        </TableCell>
-                        <TableCell
-                          sx={{
-                            backgroundColor: "#d41c2c",
-                            color: "white",
-                            fontSize: 20,
-                          }}
-                        >
-                          Type
-                        </TableCell>
-                        <TableCell
-                          sx={{
-                            backgroundColor: "#d41c2c",
-                            color: "white",
-                            fontSize: 20,
-                          }}
-                        >
-                          Status
-                        </TableCell>
-                      </TableRow>
-                    </TableHead>
-
-                    <TableBody>
-                      {txn.map((item) => (
-                        <TableRow key={item.txnId}>
-                          <TableCell sx={{ fontSize: 18, padding: 2 }}>
-                            {item.timeStamp}
+      <Box
+        // container
+        display="flex"
+        alignItems="center"
+        justifyContent="flex-start"
+        // minHeight="100vh"
+      >
+        {transactions.length > 0 ? (
+          <div style={{ margin: "70px" }}>
+            {transactions.map((txn, index) => (
+              <div key={index}>
+                <h2 style={{ display: "flex", flex: "start" }}>
+                  🚩Transactions for Account Number : {accounts[index]}
+                </h2>
+                {txn.length > 0 ? (
+                  <TableContainer component={Paper}>
+                    <Table>
+                      <TableHead>
+                        <TableRow>
+                          <TableCell
+                            sx={{
+                              backgroundColor: "#d41c2c",
+                              color: "white",
+                              fontSize: 20,
+                            }}
+                          >
+                            Time Stamp
                           </TableCell>
-                          <TableCell sx={{ fontSize: 18, padding: 2 }}>
-                            {item.amt}
+                          <TableCell
+                            sx={{
+                              backgroundColor: "#d41c2c",
+                              color: "white",
+                              fontSize: 20,
+                            }}
+                          >
+                            Amount
                           </TableCell>
-                          <TableCell sx={{ fontSize: 18, padding: 2 }}>
-                            {item.txnType}
+                          <TableCell
+                            sx={{
+                              backgroundColor: "#d41c2c",
+                              color: "white",
+                              fontSize: 20,
+                            }}
+                          >
+                            Type
                           </TableCell>
-                          <TableCell sx={{ fontSize: 18, padding: 2 }}>
-                            {item.status}
+                          <TableCell
+                            sx={{
+                              backgroundColor: "#d41c2c",
+                              color: "white",
+                              fontSize: 20,
+                            }}
+                          >
+                            Status
                           </TableCell>
                         </TableRow>
-                      ))}
-                    </TableBody>
-                  </Table>
-                </TableContainer>
-              ) : (
-                <h3
-                  style={{
-                    display: "flex",
-                    flex: "start",
+                      </TableHead>
 
-                    color: "#073717",
-                    fontWeight: 600,
-                  }}
-                >
-                  No Transactions🥳
-                </h3>
-              )}
-            </div>
-          ))}
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
+                      <TableBody>
+                        {txn.map((item) => (
+                          <TableRow key={item.txnId}>
+                            <TableCell sx={{ fontSize: 18, padding: 2 }}>
+                              {item.timeStamp}
+                            </TableCell>
+                            <TableCell sx={{ fontSize: 18, padding: 2 }}>
+                              {item.amt}
+                            </TableCell>
+                            <TableCell sx={{ fontSize: 18, padding: 2 }}>
+                              {item.txnType}
+                            </TableCell>
+                            <TableCell sx={{ fontSize: 18, padding: 2 }}>
+                              {item.status}
+                            </TableCell>
+                          </TableRow>
+                        ))}
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
+                ) : (
+                  <h3
+                    style={{
+                      display: "flex",
+                      flex: "start",
+
+                      color: "#073717",
+                      fontWeight: 600,
+                    }}
+                  >
+                    No Transactions🥳
+                  </h3>
+                )}
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </Box>
     </div>
   );
 }
