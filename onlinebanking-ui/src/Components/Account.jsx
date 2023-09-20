5f1d8b1c0c3befc9dcbf252a77187f2a5be203d0
gowsiman <gowsimanarg19@gmail.com>
2023-09-20 19:03:42 +0530

    Admin application reverted back into onlinebanking-ui

--- onlinebanking-ui/src/Components/Account.jsx
+++ onlinebanking-ui/src/Components/Account.jsx
@@ -1,51 +1,45 @@
 import { Button, Dialog, DialogTitle } from "@mui/material";
 import { Typography } from "@mui/material";
 import axios from "axios";
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { enqueueSnackbar } from "notistack";
 export default function (props) {
-	const account = props.account;
-	const [isStrikethrough, setIsStrikethrough] = useState(false);
-	const [transactions, setTransactions] = useState([]);
-	const [showTxns, setShowTxns] = useState(false);
+    const account = props.account
+    const [deleted, setDeleted] = useState(false)
+    const [disabled, setDisabled] = useState(account.disabled || account.balance < 1000)
 
-	const handleClick = async (event) => {
-		const accNo = event.target.name;
-		if (props.action === "View") {
-			try {
-				const response = await axios.get(
-					`http://localhost:8080/getTransactions/${accNo}`
-				);
-				props.setTransactions(response.data);
-				setShowTxns(true);
-				console.log(response.data);
-			} catch (error) {
-				enqueueSnackbar(`An error occured: ${error.message}`, "error");
-			}
-		} else {
-			setIsStrikethrough(!isStrikethrough);
-			props.handle(accNo);
-		}
-	};
+    useEffect(()=>{}, [deleted, disabled])
 
-	return (
-		<>
-			<Typography
-				key={account.accNo}
-				color={props.action === "Disable" && isStrikethrough && "textSecondary"}
-				style={{
-					textDecoration:
-						isStrikethrough && props.action === "Delete"
-							? "line-through"
-							: "none",
-				}}
-			>
-				{account.accType} - {account.accNo} - {account.balance}
-				<Button name={account.accNo} onClick={handleClick}>
-					{props.action}
-				</Button>
-			</Typography>
-		</>
-	);
+    const handleClick = async (event) =>  {
+        const accNo = event.target.name;
+        if (props.action === "View"){
+            try{
+                const response = await axios.get(`http://localhost:8080/getTransactions/${accNo}`)
+                props.setTransactions(response.data)
+                props.clicked(true)
+            }catch(error){
+                enqueueSnackbar(`An error occured: ${error.message}`, "error");
+            }
+        }
+        else if (props.action === "Delete"){
+            setDeleted(true)
+        }
+        else if (props.action === "Disable") {
+            setDisabled(!disabled)
+        }
+        props.handle(accNo)
+    }
+        
+    return (<>
+         <Typography key={account.accNo} 
+         color={props.action === 'Disable' && disabled && "textSecondary"}
+         style={{ textDecoration: (deleted && props.action === 'Delete') ? 'line-through' : 'none' }}
+         >
+         {account.accType} - {account.accNo} - {account.balance} 
+         <Button name={account.accNo} onClick={handleClick}>{props.action == "Disable" ? 
+                                                            (disabled ? "Enable" : "Disable")
+                                                            : props.action}</Button></Typography>
+        </>
+        )
 }
